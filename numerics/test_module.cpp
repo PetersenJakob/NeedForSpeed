70c2df6d1848cabc12b6723f7d48cee9761c9a21
jkbptrsn <petersen.jakob@icloud.com>
2024-01-18 22:05:51 +0100

    Simple convergence tests based on max- and l2-norm.

--- numerics/test_module.cpp
+++ numerics/test_module.cpp
@@ -18,10 +18,10 @@ std::vector<double> vector_diff(
 
 double max_norm(std::vector<double> vec);
 
-double l2_norm(std::vector<double> vec);
+double l2_norm(const double dx, std::vector<double> vec);
 
 
-void test_exp(const std::vector<double>& grid, const int deriv_order, BandDiagonal& deriv_operator);
+std::vector<double> test_exp(const std::vector<double>& grid, const int deriv_order, BandDiagonal& deriv_operator, bool screen = false);
 
 void test_cos(const std::vector<double>& grid, const int deriv_order, BandDiagonal& deriv_operator);
 
@@ -42,6 +42,7 @@ int main() {
 	const double dx_exp = grid_exp[1] - grid_exp[0];
 	const double dx_cos = grid_cos[1] - grid_cos[0];
 
+
 	
 	TriDiagonal d1_c2b1_exp = d1dx1::c2b1(order, dx_exp);
 	TriDiagonal d1_c2b1_cos = d1dx1::c2b1(order, dx_cos);
@@ -61,11 +62,28 @@ int main() {
 	PentaDiagonal d2_c4b4_exp = d2dx2::c4b4(order, dx_exp);
 	PentaDiagonal d2_c4b4_cos = d2dx2::c4b4(order, dx_cos);
 
-	test_exp(grid_exp, 1, d1_c4b4_exp);
+//	test_exp(grid_exp, 1, d1_c4b4_exp);
 
-	test_cos(grid_cos, 1, d1_c2b2_cos);
+//	test_cos(grid_cos, 1, d1_c2b2_cos);
 
-	test_cos(grid_cos, 1, d1_c4b4_cos);
+//	test_cos(grid_cos, 1, d1_c4b4_cos);
+
+
+	for (int i = 0; i != 3; ++i) {
+	
+		const int n_points = 20 * pow(2, i);
+		const double x_min = -0.2;
+		const double x_max = 0.2;
+
+		std::vector<double> grid = grid_equidistant(x_min, x_max, n_points);
+
+		const double dx = grid[1] - grid[0];
+
+		TriDiagonal d1_c2b1 = d1dx1::c2b1(n_points, dx);
+
+		test_exp(grid, 1, d1_c2b1, true);
+	
+	}
 
 
 	std::vector<double> column_tmp(order, 0.0);
@@ -122,7 +140,7 @@ int main() {
 }
 
 
-void test_exp(const std::vector<double>& grid, const int deriv_order, BandDiagonal& deriv_operator) {
+std::vector<double> test_exp(const std::vector<double>& grid, const int deriv_order, BandDiagonal& deriv_operator, bool screen) {
 
 	const int order = grid.size();
 
@@ -138,14 +156,38 @@ void test_exp(const std::vector<double>& grid, const int deriv_order, BandDiagon
 
 	std::vector<double> deriv_fd = deriv_operator.mat_vec_prod(func);
 
-	std::cout << "Test derivative of exp function:" << std::endl << "Derivative order = " << deriv_order << std::endl;
+	if (screen) {
+		std::cout << "Test derivative of exp function:" << std::endl << "Derivative order = " << deriv_order << std::endl;
+	}
 
 	if (deriv_order == 1) {
-		print_test(grid, func, deriv1, deriv_fd);
+
+		std::vector<double> diff = vector_diff(deriv1, deriv_fd);
+		double max_norm_ = max_norm(diff);
+		double dx = grid[1] - grid[0];
+		double l2_norm_ = l2_norm(dx, diff);
+
+		if (screen) {
+//			print_test(grid, func, deriv1, deriv_fd);
+			std::cout << "max-norm = " << max_norm_ << "\t l2-norm = " << l2_norm_ << std::endl;
+		}
+
 	}
 	else if (deriv_order == 2) {
-		print_test(grid, func, deriv2, deriv_fd);
+
+		std::vector<double> diff = vector_diff(deriv2, deriv_fd);
+		double max_norm_ = max_norm(diff);
+		double dx = grid[1] - grid[0];
+		double l2_norm_ = l2_norm(dx, diff);
+
+		if (screen) {
+//			print_test(grid, func, deriv2, deriv_fd);
+			std::cout << "max-norm = " << max_norm_ << "\t l2-norm = " << l2_norm_ << std::endl;
+		}
+
 	}
+
+	return deriv_fd;
 
 }
 
@@ -222,7 +264,7 @@ std::vector<double> vector_diff(
 double max_norm(std::vector<double> vec) {
 
 	// Absolue value of each element.
-	std::for_each(vec.begin(), vec.end(), [](double x) { return abs(x); });
+	std::transform(vec.begin(), vec.end(), vec.begin(), [](double x) { return abs(x); });
 
 	return *std::max_element(vec.begin(), vec.end());
 
@@ -230,11 +272,11 @@ double max_norm(std::vector<double> vec) {
 
 
 // l2-norm (vector norm). TODO: Should this be dependent on dx? Like a "Riemann sum" expression?
-double l2_norm(std::vector<double> vec) {
+double l2_norm(const double dx, std::vector<double> vec) {
 
 	// Square of each element.
-	std::for_each(vec.begin(), vec.end(), [](double x) { return pow(x, 2); });
+	std::transform(vec.begin(), vec.end(), vec.begin(), [dx](double x) { return dx * pow(x, 2); });
 
-	return std::accumulate(vec.begin(), vec.end(), (double)0);
+	return sqrt(std::accumulate(vec.begin(), vec.end(), (double)0));
 
 }
--- unit_test/pch.h
+++ unit_test/pch.h
@@ -4,4 +4,17 @@
 
 #pragma once
 
+#define _USE_MATH_DEFINES
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+#include "band_diagonal_matrix.h"
+#include "finite_difference.h"
+#include "grid_generator.h"
+#include "tridiagonal_matrix_solver.h"
+
 #include "gtest/gtest.h"
