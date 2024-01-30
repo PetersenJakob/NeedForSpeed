e355a1f346119b63e4da02859d5cce3a2a918f70
jkbptrsn <petersen.jakob@icloud.com>
2024-01-30 21:23:01 +0100

    Separate tridiagonal-matrix-solver function with three diagonals and column vector as parameters.

--- numerics/tridiagonal_matrix_solver.cpp
+++ numerics/tridiagonal_matrix_solver.cpp
@@ -5,7 +5,7 @@
 #include "tridiagonal_matrix_solver.h"
 
 void tri_solver(TriDiagonal& matrix, std::vector<double>& column) {
-	
+
 #if false
 	print_matrix(matrix);
 	std::cout << "Column vector before: " << std::endl;
@@ -14,8 +14,10 @@ void tri_solver(TriDiagonal& matrix, std::vector<double>& column) {
 	}
 	std::cout << std::endl;
 #endif
+
 //	matrix.adjust_boundary
 	matrix.adjust_boundary(column);
+
 #if false
 	print_matrix(matrix);
 	std::cout << "Column vector after: " << std::endl;
@@ -24,36 +26,69 @@ void tri_solver(TriDiagonal& matrix, std::vector<double>& column) {
 	}
 	std::cout << std::endl;
 #endif
+
+	tridiagonal_matrix_solver(
+		matrix.matrix[0],
+		matrix.matrix[1],
+		matrix.matrix[2],
+		column);
+
+}
+
+
+void tridiagonal_matrix_solver(
+	std::vector<double>& sub,
+	std::vector<double>& main,
+	std::vector<double>& super,
+	std::vector<double>& column) {
+
 	// Number of elements along main diagonal.
-	const int n_elements = column.size();
+	const int n_elements = main.size();
 
-	std::vector<double> tmp(n_elements, 0.0);
+	// Store modified elements of super-diagonal.
+	std::vector<double> super_tmp(n_elements, 0.0);
 
-	// Forward sweep.
-	double denominator = matrix.matrix[1][0];
+	// Temporary index.
+	int idx_tmp = 0;
 
-	tmp[0] = matrix.matrix[2][0] / denominator;
+	// *****************************************************************
+	// Forward sweep. Remove sub-diagonal elements by Gauss elimination.
+	// *****************************************************************
+
+	// Denominator for normalization of 1st element of main diagonal.
+	double denominator = main[0];
+
+	// 1st element of super-diagonal after normalization.
+	super_tmp[0] = super[0] / denominator;
+
+	// 1st element of column matrix after normalization.
 	column[0] /= denominator;
 
-	int idx_tmp;
 	for (int i = 1; i != n_elements; ++i) {
 
 		idx_tmp = i - 1;
 
-		denominator = matrix.matrix[1][i] - matrix.matrix[0][i] * tmp[idx_tmp];
+		// Denominator for normalization of (i + 1)'th element of main diagonal.
+		denominator = main[i] - sub[i] * super_tmp[idx_tmp];
 
-		tmp[i] = matrix.matrix[2][i] / denominator;
+		// (i + 1)'th element of super-diagonal after Gauss elimination.
+		super_tmp[i] = super[i] / denominator;
 
-		column[i] = (column[i] - matrix.matrix[0][i] * column[idx_tmp]) / denominator;
+		// (i + 1)'th element of column matrix after Gauss elimination.
+		column[i] = (column[i] - sub[i] * column[idx_tmp]) / denominator;
 
 	}
 
-	// Back substitution.
+	// ***********************************************************************
+	// Back substitution. Remove super-diagonal elements by Gauss elimination.
+	// ***********************************************************************
+
 	for (int i = n_elements - 2; i != -1; --i) {
 
 		idx_tmp = i + 1;
 
-		column[i] -= tmp[i] * column[idx_tmp];
+		// (i + 1)'th element of column matrix after Gauss elimination.
+		column[i] -= super_tmp[i] * column[idx_tmp];
 
 	}
 
--- numerics/tridiagonal_matrix_solver.h
+++ numerics/tridiagonal_matrix_solver.h
@@ -6,3 +6,10 @@
 
 
 void tri_solver(TriDiagonal& matrix, std::vector<double>& column);
+
+
+void tridiagonal_matrix_solver(
+	std::vector<double>& sub,
+	std::vector<double>& main, 
+	std::vector<double>& super, 
+	std::vector<double>& column);
