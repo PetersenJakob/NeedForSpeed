e54b56879a2920e46874b3ada45e1531cf88cfac
jkbptrsn <petersen.jakob@icloud.com>
2024-02-06 19:35:02 +0100

    Namespace (grid) added to grid_generator.h.

--- numerics/grid_generator.cpp
+++ numerics/grid_generator.cpp
@@ -1,6 +1,9 @@
 #include <Eigen/Dense>
 #include <vector>
 
+#include "grid_generator.h"
+
+
 // TODO: "Doc string" for function
 // TODO: Description of parameters and return value type
 std::vector<double> grid_equidistant(const double x_min, const double x_max, const int n_points) {
@@ -17,16 +20,38 @@ std::vector<double> grid_equidistant(const double x_min, const double x_max, con
 	return grid;
 }
 
-Eigen::VectorXd grid_eigen(const double x_min, const double x_max, const int n_points) {
 
-	// Constant step size.
+std::vector<double> grid::equidistant(
+	const double x_min, 
+	const double x_max, 
+	const int n_points) {
+
 	const double dx = (x_max - x_min) / (n_points - 1);
 
-	//
-	Eigen::VectorXd grid(n_points);
+	std::vector<double> grid(n_points);
 
-	for (int n = 0; n != n_points; ++n)
-		grid(n) = dx * n + x_min;
+	for (int i = 0; i != n_points; ++i) {
+		grid[i] = dx * i + x_min;
+	}
 
 	return grid;
+
+}
+
+
+Eigen::VectorXd grid::eigen::uniform(
+	const double x_min, 
+	const double x_max, 
+	const int n_points) {
+
+	const double dx = (x_max - x_min) / (n_points - 1);
+
+	Eigen::VectorXd grid(n_points);
+
+	for (int i = 0; i != n_points; ++i) {
+		grid(i) = dx * i + x_min;
+	}
+
+	return grid;
+
 }
