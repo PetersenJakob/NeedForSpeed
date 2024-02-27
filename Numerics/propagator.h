5951f0d0314b675d25890c6a216e0789c92ddb99
jkbptrsn <petersen.jakob@icloud.com>
2024-02-27 11:47:06 +0100

    Implementation of 2-dimensional Douglas-Rachford and Graig-Sneyd propagators.

--- Numerics/propagator.h
+++ Numerics/propagator.h
@@ -12,30 +12,62 @@
 namespace propagator {
 
 	// Theta scheme.
-	// Andersen and Piterbarg, REF...
-	template <class T>
-	void theta_1d(
-		const double dt,
-		const T& identity,
-		const T& derivative,
-		std::vector<double>& func,
-		const double theta = 0.5) {
+	// References
+	// - AP: Andersen and Piterbarg (2010).
+	namespace theta_1d {
 
-		// Left-hand-side operator, Eq. ().
-		T lhs = derivative;
-		lhs *= - theta * dt;
-		lhs += identity;
+		template <class T>
+		void step_1(
+			const double dt,
+			const T& identity,
+			const T& derivative,
+			std::vector<double>& func,
+			const double theta = 0.5) {
 
-		// Right-hand-side operator, Eq. ().
-		T rhs = derivative;
-		rhs *= (1.0 - theta) * dt;
-		rhs += identity;
+			// AP Eq. (2.18), right-hand-side operator.
+			// Operator evaluated at time t + dt (see AP Remarks 2.2.4).
+			T rhs = derivative;
+			rhs *= (1.0 - theta) * dt;
+			rhs += identity;
 
-		// Evaluation of righ-hand-side.
-		func = rhs * func;
+			func = rhs * func;
 
-		// Solve matrix equation.
-		solver::band(lhs, func);
+		}
+
+		template <class T>
+		void step_2(
+			const double dt,
+			const T& identity,
+			const T& derivative,
+			std::vector<double>& func,
+			const double theta = 0.5) {
+
+			// AP Eq. (2.18), right-hand-side operator.
+			// Operator evaluated at time t (see AP Remarks 2.2.4).
+			T lhs = derivative;
+			lhs *= -theta * dt;
+			lhs += identity;
+
+			// Solve matrix equation.
+			solver::band(lhs, func);
+
+		}
+
+		template <class T>
+		void full(
+			const double dt,
+			const T& identity,
+			const T& derivative,
+			std::vector<double>& func,
+			const double theta = 0.5) {
+
+			// Step one is carried out at time t + dt.
+			step_1(dt, identity, derivative, func, theta);
+
+			// Step one is carried out at time t.
+			step_2(dt, identity, derivative, func, theta);
+
+		}
 
 	}
 
@@ -43,6 +75,8 @@ namespace propagator {
 	namespace adi {
 
 		// Douglas-Rachford 2-dimensional scheme.
+		// References
+		// - AP: Andersen and Piterbarg (2010).
 		template <class T1, class T2>
 		void dr_2d(
 			const double dt,
@@ -53,53 +87,56 @@ namespace propagator {
 			std::vector<std::vector<double>>& func,
 			const double theta = 0.5) {
 
+			// TODO: Change func to ordinary std::vector<double>?
+
 			const int n_points_1 = identity_1.order();
 			const int n_points_2 = identity_2.order();
 
 			std::vector<double> inner(n_points_2, 0.0);
 			std::vector<std::vector<double>> func_tmp(n_points_1, inner);
 
-			// Eq. (2.68), right-hand-side.
-			T2 lhs_2 = derivative_2;
-			lhs_2 *= dt;
+			std::vector<double> func_strip(n_points_1, 0.0);
+
+			// AP Eq. (2.68), right-hand-side.
+			T2 rhs_2 = derivative_2;
+			rhs_2 *= dt;
+
 			for (int i = 0; i != n_points_1; ++i) {
-				func_tmp[i] = lhs_2 * func[i];
+				func_tmp[i] = rhs_2 * func[i];
 			}
 
-			T1 lhs_1 = derivative_1;
-			lhs_1 *= (1.0 - theta) * dt;
-			lhs_1 += identity_1;
-
-			for (int i = 0; i != n_points_2; ++i) {
-
-				std::vector<double> func_strip(n_points_1, 0.0);
-
-				for (int j = 0; j != n_points_1; ++i) {
-					func_strip[j] = func[j][i];
-				}
-
-				func_strip = lhs_1 * func_strip;
-
-				for (int j = 0; j != n_points_1; ++i) {
-					func[j][i] = func_tmp[j][i] + func_strip[j];
-				}
-
-			}
-
-			// Eq. (2.68), left-hand-side.
 			T1 rhs_1 = derivative_1;
-			rhs_1 *= -theta * dt;
+			rhs_1 *= (1.0 - theta) * dt;
 			rhs_1 += identity_1;
 
 			for (int i = 0; i != n_points_2; ++i) {
-			
+
 				std::vector<double> func_strip(n_points_1, 0.0);
 
 				for (int j = 0; j != n_points_1; ++i) {
 					func_strip[j] = func[j][i];
 				}
 
-				solver::band(rhs_1, func_strip);
+				func_strip = rhs_1 * func_strip;
+
+				for (int j = 0; j != n_points_1; ++i) {
+					func[j][i] = func_strip[j] + func_tmp[j][i];
+				}
+
+			}
+
+			// AP Eq. (2.68), left-hand-side.
+			T1 lhs_1 = derivative_1;
+			lhs_1 *= - theta * dt;
+			lhs_1 += identity_1;
+
+			for (int i = 0; i != n_points_2; ++i) {
+			
+				for (int j = 0; j != n_points_1; ++i) {
+					func_strip[j] = func[j][i];
+				}
+
+				solver::band(lhs_1, func_strip);
 
 				for (int j = 0; j != n_points_1; ++i) {
 					func[j][i] = func_strip[j];
@@ -107,25 +144,27 @@ namespace propagator {
 
 			}
 
-			// Eq. (2.69), left-hand-side.
+			// AP Eq. (2.69), right-hand-side.
 			for (int i = 0; i != n_points_1; ++i) {
 				for (int j = 0; j != n_points_2; ++j) {
 					func[i][j] -= theta * func_tmp[i][j];
 				}
 			}
 
-			// Eq. (2.69), left-hand-side.
-			T2 rhs_2 = derivative_2;
-			rhs_2 *= -theta;
-			rhs_2 += identity_2;
+			// AP Eq. (2.69), left-hand-side.
+			T2 lhs_2 = derivative_2;
+			lhs_2 *= - theta * dt;
+			lhs_2 += identity_2;
 
 			for (int i = 0; i != n_points_1; ++i) {
-				solver::band(rhs_2, func[i]);
+				solver::band(lhs_2, func[i]);
 			}
 
 		}
 
 		// Craig-Sneyd 2-dimensional scheme.
+		// References
+		// - AP: Andersen and Piterbarg (2010).
 		template <class T1, class T2>
 		void cs_2d(
 			const double dt,
@@ -135,12 +174,13 @@ namespace propagator {
 			const T2& derivative_2,
 
 			std::function<std::vector<std::vector<double>>(
-				T1&, 
-				T2&,
 				std::vector<std::vector<double>>)> derivative_12,
 
+			std::vector<std::vector<double>>& func,
+
 			const double theta = 0.5,
-			const double lambda = 0.5) {
+			const double lambda = 0.5,
+			const int n_iterations = 1) {
 
 			const int n_points_1 = identity_1.order();
 			const int n_points_2 = identity_2.order();
@@ -148,86 +188,135 @@ namespace propagator {
 			std::vector<double> inner(n_points_2, 0.0);
 			std::vector<std::vector<double>> func_tmp_1(n_points_1, inner);
 			std::vector<std::vector<double>> func_tmp_2(n_points_1, inner);
+			std::vector<std::vector<double>> func_tmp_3(n_points_1, inner);
 
-			// ###############
-			// Predictor step.
-			// ###############
+			std::vector<double> func_strip(n_points_1, 0.0);
 
-			// Eq. (2.68), right-hand-side.
-			T2 lhs_2 = derivative_2;
-			lhs_2 *= dt;
-			for (int i = 0; i != n_points_1; ++i) {
-				func_tmp_1[i] = lhs_2 * func[i];
-			}
+			// ##########
+			// Operators.
+			// ##########
 
-
-			// TODO: Should be d1dx1_1 and d1dx1_2!
-			// TODO: What about prefix-function?
-			func_tmp_2 = d2dxdy(derivative_1, derivative_2, func);
-
-
-			T1 lhs_1 = derivative_1;
-			lhs_1 *= (1.0 - theta) * dt;
-			lhs_1 += identity_1;
-
-			for (int i = 0; i != n_points_2; ++i) {
-
-				std::vector<double> func_strip(n_points_1, 0.0);
-
-				for (int j = 0; j != n_points_1; ++i) {
-					func_strip[j] = func[j][i];
-				}
-
-				func_strip = lhs_1 * func_strip;
-
-				for (int j = 0; j != n_points_1; ++i) {
-					func[j][i] = func_tmp_1[j][i] + func_tmp_2[j][i] + func_strip[j];
-				}
-
-			}
-
-			// Eq. (2.68), left-hand-side.
+			// AP Eq. (2.88) and (2.90), right-hand-side.
 			T1 rhs_1 = derivative_1;
-			rhs_1 *= -theta * dt;
+			rhs_1 *= (1.0 - theta) * dt;
 			rhs_1 += identity_1;
 
-			for (int i = 0; i != n_points_2; ++i) {
-
-				std::vector<double> func_strip(n_points_1, 0.0);
-
-				for (int j = 0; j != n_points_1; ++i) {
-					func_strip[j] = func[j][i];
-				}
-
-				solver::band(rhs_1, func_strip);
-
-				for (int j = 0; j != n_points_1; ++i) {
-					func[j][i] = func_strip[j];
-				}
-
-			}
-
-			// Eq. (2.69), left-hand-side.
-			for (int i = 0; i != n_points_1; ++i) {
-				for (int j = 0; j != n_points_2; ++j) {
-					func[i][j] -= theta * func_tmp_1[i][j];
-				}
-			}
-
-			// Eq. (2.69), left-hand-side.
 			T2 rhs_2 = derivative_2;
-			rhs_2 *= - theta;
-			rhs_2 += identity_2;
+			rhs_1 *= dt;
 
-			for (int i = 0; i != n_points_1; ++i) {
-				solver::band(rhs_2, func[i]);
+			// AP Eq. (2.88) and (2.90), left-hand-side.
+			T1 lhs_1 = derivative_1;
+			lhs_1 *= - theta * dt;
+			lhs_1 += identity_1;
+
+			T2 lhs_2 = derivative_2;
+			lhs_2 *= - theta * dt;
+			lhs_2 += identity_2;
+
+			for (int n = 0; n != n_interations; ++n) {
+
+				// ###############
+				// Predictor step.
+				// ###############
+
+				// AP Eq. (2.88), right-hand-side.
+				for (int i = 0; i != n_points_2; ++i) {
+
+					for (int j = 0; j != n_points_1; ++j) {
+						func_strip[j] = func[j][i];
+					}
+
+					func_strip = rhs_1 * func_strip;
+
+					for (int j = 0; j != n_points_1; ++j) {
+						func_tmp_1[j][i] = func_strip[j];
+					}
+
+				}
+
+				for (int i = 0; i != n_points_1; ++i) {
+					func_tmp_2[i] = rhs_2 * func[i];
+				}
+
+				func_tmp_3 = derivative_12(func);
+
+				for (int i = 0; i != n_points_1; ++i) {
+					for (int j = 0; j != n_points_2; ++j) {
+						func[i][j] = func_tmp_1[i][j] + func_tmp_2[i][j] 
+							+ dt * func_tmp_3[i][j];
+					}
+				}
+
+				// AP Eq. (2.88), left-hand-side.
+				for (int i = 0; i != n_points_2; ++i) {
+
+					for (int j = 0; j != n_points_1; ++j) {
+						func_strip[j] = func[j][i];
+					}
+
+					solver::band(lhs_1, func_strip);
+
+					for (int j = 0; j != n_points_1; ++j) {
+						func[j][i] = func_strip[j];
+					}
+
+				}
+
+				// AP Eq. (2.89), right-hand-side.
+				for (int i = 0; i != n_points_1; ++i) {
+					for (int j = 0; j != n_points_2; ++j) {
+						func[i][j] -= theta * func_tmp_2[i][j];
+					}
+				}
+
+				// AP Eq. (2.89), left-hand-side.
+				for (int i = 0; i != n_points_1; ++i) {
+					solver::band(lhs_2, func[i]);
+				}
+
+				// ###############
+				// Corrector step.
+				// ###############
+
+				// AP Eq. (2.90), right-hand-side.
+				func = derivative_12(func);
+
+				for (int i = 0; i != n_points_1; ++i) {
+					for (int j = 0; j != n_points_2; ++j) {
+						func[i][j] *= lambda * dt;
+						func[i][j] = func_tmp_1[i][j] + func_tmp_2[i][j]
+							+ (1.0 - lambda) * dt * func_tmp_3[i][j];
+					}
+				}
+
+				// AP Eq. (2.90), left-hand-side.
+				for (int i = 0; i != n_points_2; ++i) {
+
+					for (int j = 0; j != n_points_1; ++j) {
+						func_strip[j] = func[j][i];
+					}
+
+					solver::band(lhs_1, func_strip);
+
+					for (int j = 0; j != n_points_1; ++j) {
+						func[j][i] = func_strip[j];
+					}
+
+				}
+
+				// AP Eq. (2.91), right-hand-side.
+				for (int i = 0; i != n_points_1; ++i) {
+					for (int j = 0; j != n_points_2; ++j) {
+						func[i][j] -= theta * func_tmp_2[i][j];
+					}
+				}
+
+				// AP Eq. (2.91), left-hand-side.
+				for (int i = 0; i != n_points_1; ++i) {
+					solver::band(lhs_2, func[i]);
+				}
+
 			}
-
-			// ###############
-			// Corrector step.
-			// ###############
-
-			 
 
 		}
 
