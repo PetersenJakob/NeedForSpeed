bb5f9266b1fefb45aa8c47496d20694ddc16c623
jkbptrsn <petersen.jakob@icloud.com>
2024-04-16 23:21:17 +0200

    A- and B-functions and their 1st order derivative.

--- Models/VasicekUtility.cpp
+++ Models/VasicekUtility.cpp
@@ -3,6 +3,46 @@
 #include "VasicekUtility.h"
 
 
+double vasicek::a_func(
+	const double time_1,
+	const double time_2,
+	const double kappa,
+	const double theta,
+	const double sigma) {
+
+	const double sigma_sq = sigma * sigma;
+
+	const double kappa_sq = kappa * kappa;
+
+	const double b = vasicek::b_func(time_1, time_2, kappa);
+
+	return (theta - sigma_sq / (2 * kappa_sq)) * (b - (time_2 - time_1))
+		- sigma_sq * b * b / (4 * kappa);
+
+}
+
+
+double vasicek::dadt_func(
+	const double time_1,
+	const double time_2,
+	const double kappa,
+	const double theta,
+	const double sigma) {
+
+	const double sigma_sq = sigma * sigma;
+
+	const double kappa_sq = kappa * kappa;
+
+	const double b = vasicek::b_func(time_1, time_2, kappa);
+
+	const double dbdt = vasicek::dbdt_func(time_1, time_2, kappa);
+
+	return (theta - sigma_sq / (2 * kappa_sq)) * (dbdt + 1)
+		- sigma_sq * b * dbdt / (2 * kappa);
+
+}
+
+
 double vasicek::b_func(
 	const double time_1,
 	const double time_2,
@@ -19,46 +59,5 @@ double vasicek::dbdt_func(
 	const double kappa) {
 
 	return -std::exp(-kappa * (time_2 - time_1));
-
-}
-
-
-double vasicek::a_func(
-	const double time_1,
-	const double time_2,
-	const double kappa,
-	const double theta,
-	const double sigma) {
-
-	const double sigma_sq = sigma * sigma;
-
-	const double kappa_sq = kappa * kappa;
-
-	const double b = vasicek::b_func(time_1, time_2, kappa);
-
-	return (theta - sigma_sq / (2 * kappa_sq)) * (b - (time_2 - time_1)) 
-		- sigma_sq * b * b / (4 * kappa);
-
-}
-
-
-double vasicek::dadt_func(
-	const double time_1,
-	const double time_2,
-	const double kappa,
-	const double theta,
-	const double sigma) {
-
-
-	const double sigma_sq = sigma * sigma;
-
-	const double kappa_sq = kappa * kappa;
-
-	const double b = vasicek::b_func(time_1, time_2, kappa);
-
-	const double dbdt = vasicek::dbdt_func(time_1, time_2, kappa);
-
-	return (theta - sigma_sq / (2 * kappa_sq)) * (dbdt + 1)
-		- 2 * sigma_sq * b * dbdt / (4 * kappa);
 
 }
--- Models/VasicekUtility.h
+++ Models/VasicekUtility.h
@@ -4,24 +4,18 @@
 namespace vasicek {
 
 	/**
-	 * @brief
-	 * @param time_1
-	 * @param time_2
-	 * @param kappa
-	 * @return
+	 * @brief Calculate A-function.
+	 * 
+	 * See Andersen & Piterbarg (2010), Proposition 10.1.4.
+	 * 
+	 * @param time_1: Initial time.
+	 * @param time_2: Time of maturity.
+	 * @param kappa: Speed of mean reversion.
+	 * @param theta: Mean reversion level.
+	 * @param sigma: Volatility.
+	 * 
+	 * @return A-function.
 	*/
-	double b_func(
-		const double time_1,
-		const double time_2,
-		const double kappa);
-
-
-	double dbdt_func(
-		const double time_1,
-		const double time_2,
-		const double kappa);
-
-
 	double a_func(
 		const double time_1,
 		const double time_2,
@@ -29,12 +23,57 @@ namespace vasicek {
 		const double theta,
 		const double sigma);
 
-
+	/**
+	 * @brief Calculate 1st order derivative of A-function wrt time_1.
+	 * 
+	 * See Andersen & Piterbarg (2010), Proposition 10.1.4.
+	 * 
+	 * @param time_1: Initial time.
+	 * @param time_2: Time of maturity.
+	 * @param kappa: Speed of mean reversion.
+	 * @param theta: Mean reversion level.
+	 * @param sigma: Volatility.
+	 *
+	 * @return Time derivative of A-function.
+	*/
 	double dadt_func(
 		const double time_1,
 		const double time_2,
 		const double kappa,
 		const double theta,
 		const double sigma);
+
+
+	/**
+	 * @brief Calculate B-function. 
+	 * 
+	 * See Andersen & Piterbarg (2010), Proposition 10.1.4.
+	 * 
+	 * @param time_1: Initial time.
+	 * @param time_2: Time of maturity.
+	 * @param kappa: Speed of mean reversion.
+	 * 
+	 * @return B-function.
+	*/
+	double b_func(
+		const double time_1,
+		const double time_2,
+		const double kappa);
+
+	/**
+	 * @brief Calculate 1st order derivative of B-function wrt time_1.
+	 * 
+	 * See Andersen & Piterbarg (2010), Proposition 10.1.4.
+	 * 
+	 * @param time_1: Initial time.
+	 * @param time_2: Time of maturity.
+	 * @param kappa: Speed of mean reversion.
+	 * 
+	 * @return Time derivative of B-function.
+	*/
+	double dbdt_func(
+		const double time_1,
+		const double time_2,
+		const double kappa);
 
 }
